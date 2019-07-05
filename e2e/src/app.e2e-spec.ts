e4e1d754aeecf03287c02a310ab20f75f90cfd7d
Allen Pierson <apierso2@my.westga.edu>
2019-07-04 20:13:03 -0400

    Installed Protractor. Deleted unneeded afterEach() test in main app test file

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -12,12 +12,4 @@ describe('workspace-project App', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('Welcome to cs6261project4!');
   });
-
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
 });
