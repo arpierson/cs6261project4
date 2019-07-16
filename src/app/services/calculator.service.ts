47eaa9b22f005c6e2d94ef79af916509b64c9e8d
Allen Pierson <apierso2@my.westga.edu>
2019-07-16 17:53:48 -0400

    Fixed one-off errors in array index calls in #calculateResistance(). Turned array into key/value

--- src/app/services/calculator.service.ts
+++ src/app/services/calculator.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
-import { stringify } from 'querystring';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculatorService {
 
-  constructor() { }
+  private resistorValues: { [key: string]: number };
 
-  calculateResistance(figure1: number, figure2: number, figure3: number, multiplier: number) {
-    var figuresConcat = '' + figure1 + figure2 + figure3; 
+  constructor() {
+    this.resistorValues = {"figure1": 0, "figure2": 0, "figure3": 0, "multiplier": 1, "tolerance": 1};
+   }
+
+  calculateResistance(): number {
+    var figuresConcat = '' + this.resistorValues[0].valueOf() + this.resistorValues[1].valueOf() + this.resistorValues[2].valueOf(); 
     var figures = +figuresConcat;
-    return figures * multiplier;
+    return figures * this.resistorValues[3].valueOf();
+  }
+
+  getTolerance(): number {
+    return this.resistorValues[4].valueOf();
   }
 }
